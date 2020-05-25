8f4a5f1797f86d9a98b33006b2dfb66109e9d1b7
Lavinya Correia <lavinya.correia01@gmail.com>
2020-05-25 19:54:11 -0300

    Inline lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,9 +18,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
+		double min = 100.0;
 		
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice() >= min);
 		for (Product p : list) {
 			System.out.println(p);
 		}
